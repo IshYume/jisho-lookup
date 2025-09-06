5fa526f91e987edfe93f16dcfd599a78a4c9f8f9
IshYume <29901587+IshYume@users.noreply.github.com>
2025-09-07 01:02:30 +0600

    inital commit - make jisho lookup functional

---
+++ jisho-lookup.js
@@ -0,0 +1,18 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "jishoLookup",
+    title: "Search on Jisho",
+    contexts: ["selection"],
+    icons: {
+      16: "icons/jisho-logo.png",
+    },
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "Search on Jisho" && info.selectionText) {
+    const query = encodeURIComponent(info.selectionText.trim());
+    const url = `https://jisho.org/search/${query}`;
+    chrome.tabs.create({ url });
+  }
+});
