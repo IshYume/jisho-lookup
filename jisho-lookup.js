aac508fcd4a57c0c424b77ca67ce4f6a5d44734e
IshYume <29901587+IshYume@users.noreply.github.com>
2025-09-07 11:59:12 +0600

    fix addon not createing tabs

--- jisho-lookup.js
+++ jisho-lookup.js
@@ -10,7 +10,7 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "Search on Jisho" && info.selectionText) {
+  if (info.menuItemId === "jishoLookup" && info.selectionText) {
     const query = encodeURIComponent(info.selectionText.trim());
     const url = `https://jisho.org/search/${query}`;
     chrome.tabs.create({ url });
